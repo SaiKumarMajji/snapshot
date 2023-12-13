24b6afd7b51efa927bdedbaa61c0870adfc37d9c
Sai Kumar Majji <saikumar170325@gmail.com>
2023-12-13 17:45:09 +0530

    Updated styles for Images component and App CSS

--- src/Images.jsx
+++ src/Images.jsx
@@ -1,28 +1,3 @@
-// import React from "react";
-
-// export default function Images({ initialImages }) {
-//   if (initialImages.length === 0) {
-//     return (
-//       <div>
-//         <h3 className="notFound">No images found</h3> <br />{" "}
-//         <p className="error">Please try with a different search term</p>
-//       </div>
-//     );
-//   }
-//   return (
-//     <div>
-//       {initialImages.map((image) => (
-//         <img
-//           className="pictures"
-//           key={image.id}
-//           src={`https://live.staticflickr.com/${image.server}/${image.id}_${image.secret}_m.jpg`}
-//           alt={image.title}
-//         />
-//       ))}
-//     </div>
-//   );
-// }
-
 import React from "react";
 
 export default function Images({ initialImages }) {
